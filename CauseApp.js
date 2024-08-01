7ebddc4f41511a105c5e14a77e15ff01aa5cb6a9
12BAIDEN <107093502+12BAIDEN@users.noreply.github.com>
2024-08-01 18:10:19 +0000

    commit

--- CauseApp.js
+++ CauseApp.js
@@ -7,7 +7,12 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect(mongodb+srv://Christopher:chriscollins@cluster0.qyupon7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0insta, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://Christopher:chriscollins@cluster0.qyupon7.mongodb.net/FundUs?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 const caseSchema = new mongoose.Schema({
   caseNumber: Number,
@@ -19,31 +24,29 @@ const causeSchema = new mongoose.Schema({
   cases: [caseSchema]
 });
 
-const Education = mongoose.model('Education', causeSchema);
-const Health = mongoose.model('Health', causeSchema);
-const Food = mongoose.model('Food', causeSchema);
+const Cause = mongoose.model('Cause', causeSchema);
 
 // Routes
 app.get('/causes', async (req, res) => {
-  const education = await Education.find({});
-  const health = await Health.find({});
-  const food = await Food.find({});
-  res.json({ education, health, food });
+  try {
+    const causes = await Cause.find({});
+    res.json(causes);
+  } catch (err) {
+    res.status(500).send('Server error');
+  }
 });
 
 app.get('/causes/:cause', async (req, res) => {
   const { cause } = req.params;
-  if (cause === 'education') {
-    const cases = await Education.find({});
-    res.json(cases);
-  } else if (cause === 'health') {
-    const cases = await Health.find({});
-    res.json(cases);
-  } else if (cause === 'food') {
-    const cases = await Food.find({});
-    res.json(cases);
-  } else {
-    res.status(404).send('Cause not found');
+  try {
+    const foundCause = await Cause.findOne({ causeName: cause });
+    if (foundCause) {
+      res.json(foundCause);
+    } else {
+      res.status(404).send('Cause not found');
+    }
+  } catch (err) {
+    res.status(500).send('Server error');
   }
 });
 
